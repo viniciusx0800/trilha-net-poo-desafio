bdd7b8773b8ed8da50b06705be294d143abc2a11
Marcos Vinicius Conceição Oliveira <vinioliveira2008y@gmail.com>
2024-09-27 15:53:06 -0300

    feat: iniciar as implementações de codigo

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo) : base(numero)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Smartphne Nokia:");
+Smartphone nokia = new Nokia(numero: "1233456", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("whattsap");
+
+System.Console.WriteLine("\n");
+
+System.Console.WriteLine("Smartphne iphone:");
+Smartphone iphone = new Iphone(numero: "05506069", modelo: "Modelo 2", imei: "99999999999", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
