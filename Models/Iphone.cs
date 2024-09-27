bc30bed7fe8e5076d46e5047e5518e4ba347bcf0
Marcos Vinicius Conceição Oliveira <vinioliveira2008y@gmail.com>
2024-09-27 16:00:33 -0300

    feat: adicionar os parametros , numero , imei, modelo e memoria

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -3,7 +3,7 @@ namespace DesafioPOO.Models
     // TODO: Herdar da classe "Smartphone"
     public class Iphone : Smartphone
     {
-        public Iphone(string numero, string modelo) : base(numero)
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero)
         {
         }
 
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -3,7 +3,7 @@ namespace DesafioPOO.Models
     // TODO: Herdar da classe "Smartphone"
     public class Nokia : Smartphone
     {
-        public Nokia(string numero, string modelo) : base(numero)
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero)
         {
         }
 
